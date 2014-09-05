97aa0f6202a953c0557856a08b6ecff7efd74bf1
Sam Newman <newmansam@outlook.com>
2014-09-05 09:45:32 +0100

    initial commit

---
+++ request-duplex.js
@@ -0,0 +1,148 @@
+var Duplex = require('readable-stream/duplex')
+var http = require('http')
+var inherits = require('inherits')
+
+function makeRequest(duplex, options) {
+  function onerror(err) {
+    duplex.emit('error', err)
+  }
+  
+  function endReq() {
+    req.removeListener('error', onerror)
+  }
+
+  var req
+  = duplex._req 
+  = http.request(options, function (res) {
+    duplex._res = res
+    duplex.emit('status', res.statusCode)
+    duplex.emit('response', res)
+    
+    function resErr(err) { onerror(err) }
+
+    function endRes() {
+      res.removeListener('data', ondata)
+      res.removeListener('error', resErr)
+      endReq()
+    }
+
+    function ondata(data) {
+      duplex.push(data)
+    }
+
+    res.once('error', resErr)
+    res.on('data', ondata)
+    res.once('end', function () {
+      duplex.push(null)
+      endRes()
+    })
+  })
+  
+  function reqErr(err) { onerror(err) }
+  req.once('error', reqErr)
+}
+
+function RequestDuplex(options) {
+  if (!(this instanceof RequestDuplex))
+    return new RequestDuplex(options)
+
+  this._options = options || {}
+  var streamOpts = {}
+  this._responded = false
+  this._fwdStatus = options.status === false ? false : true
+  this._fwdHeaders = options.headers === false ? false : true
+  this._statusCode = 0
+  this._pipping = []
+
+  if (this._options.objectMode === true) {
+    streamOpts.objectMode = true
+    delete this._options.objectMode
+  }
+
+  Duplex.call(this, streamOpts)
+}
+
+inherits(RequestDuplex, Duplex)
+
+module.exports = RequestDuplex
+
+Object.defineProperty(RequestDuplex.prototype, 'statusCode', {
+  get: function () { return this._res && this._res.statusCode }
+})
+
+Object.defineProperty(RequestDuplex.prototype, 'responded', {
+  get: function () { return !!this._res }
+})
+
+function transEnc(options) {
+  options.headers = options.headers || {}
+  options.headers['Transfer-Encoding'] = 'chunked'
+}
+
+function maybeMakeReq(duplex, data) {
+  if (!duplex._req) {
+    if (data) transEnc(duplex._options)
+    makeRequest(duplex, duplex._options)
+  }
+}
+
+RequestDuplex.prototype._write = function (data, enc, next) {
+  maybeMakeReq(this, data)
+  this._req.write(data)
+  next()
+}
+
+var end = RequestDuplex.prototype.end
+RequestDuplex.prototype.end = function (data, enc) {
+  maybeMakeReq(this, data)
+  end.call(this, data, enc)
+  this._req.end()
+}
+
+RequestDuplex.prototype._read = function (n) {
+  return this._res && this._res.read(n)
+}
+
+function extend(a, b) {
+  for (var i in b) a[i] = b[i]
+  return a
+}
+
+var pipe = RequestDuplex.prototype.pipe
+RequestDuplex.prototype.pipe = function (writable) {
+  if (this._fwdStatus === true) {
+    if (this.responded) {
+      Object.defineProperty(writable, 'statusCode', {
+        get: function () { return code }
+      })
+    } else {
+      this.once('status', function (code) {
+        Object.defineProperty(writable, 'statusCode', {
+          get: function () { return code }
+        })
+      })
+    }
+  }
+
+  if (this._fwdHeaders === true) {
+    writable.headers = writable.headers || {}
+    if (this.responded) {
+      writable.headers = extend(writable.headers, this._res.headers)
+    } else {
+      this.on('response', function (res) {
+        writable.headers = extend(writable.headers, res.headers)
+      })
+    }
+  }
+
+  pipe.call(this, writable)
+}
+
+var unpipe = RequestDuplex.prototype.unpipe
+RequestDuplex.prototype.unpipe = function (str) {
+  var i
+  while ((i = this._pipping.indexOf(str)) > -1)
+    this._pipping.splice(i, 1)
+
+  unpipe.call(this, str)
+}
---
+++ test.js
@@ -0,0 +1,167 @@
+var RequestDuplex = require('./')
+var should = require('should')
+var sinon = require('sinon')
+var stream = require('readable-stream')
+var http = require('http')
+
+describe('RequestDuplex!', function () {
+
+  beforeEach(function () {
+    var self = this
+    var i = 0
+    self.data = [
+      'some'
+      , 'data'
+    ]
+
+    self.input = new stream.Readable()
+    self.input._read = function () {
+      i < self.data.length
+        ? this.push(self.data[i++])
+        : this.push(null);
+    }
+    
+    self.result = ''
+    self.output = new stream.Writable()
+    self.output._write = function (chunk, enc, next) {
+      if (chunk) self.result += chunk
+      next()
+    }
+
+  })
+
+  it('Should handle successful request', function (done) {
+    var self = this
+    var responseData = "response from webserver"
+    var responseCode = 200
+
+    var server = http.createServer(function (req, res) {
+      var incomingData = "";
+      req.on('data', function (d) {
+        incomingData += d
+      })
+      req.on('end', function () {
+        incomingData.should.equal(self.data.join(''))
+      })
+
+      res.statusCode = responseCode
+      res.end(responseData)
+    })
+    
+    var port
+    server.listen(function () {
+      port = server.address().port
+      
+      var options = {
+        port: port
+        , method: "GET"
+      }
+      
+      var r = new RequestDuplex(options)
+
+      self.output.on('finish', function () {
+        self.result.should.equal(responseData)
+        self.output.statusCode.should.equal(200)
+        r.statusCode.should.equal(200)
+        done();
+        server.close()
+      })
+
+      r.on('error', function (err) {
+        console.error(err)
+      })
+      
+      self.input.pipe(r).pipe(self.output)
+    })
+  })
+  
+  it('Should take a stream piped into it', function (done) {
+    var responseData = "response from webserver"
+    var responseCode = 500
+
+    var server = http.createServer(function (req, res) {
+      res.statusCode = responseCode
+      res.end(responseData)
+    })
+    
+    var port
+    var self = this
+    server.listen(function () {
+      port = server.address().port
+      
+      var options = {
+        port: port
+        , method: "GET"
+      }
+      
+      // should still work without new
+      var r = RequestDuplex(options)
+
+      self.output.on('finish', function () {
+        self.output.statusCode.should.equal(500)
+        r.statusCode.should.equal(500)
+        done();
+        server.close()
+      })
+      
+      self.input.pipe(r).pipe(self.output)
+    })
+  })
+  
+  it('Should emit error on request error', function (done) {
+    var responseData = "response from webserver"
+    var options = {
+      port: 38711
+      , method: "GET"
+    }
+      
+    // should still work without new
+    var r = RequestDuplex(options)
+    r.on('error', function (err) {
+      (err instanceof Error).should.equal(true)
+      done()
+    })
+
+    this.input.pipe(r).pipe(this.output)
+  })
+  
+  it('Should take a stream piped into it', function (done) {
+    var mockRequest = new stream.Writable()
+    mockRequest._write = function (d, e, n) { n() }
+    var mockResponse = new stream.Writable()
+    
+    sinon.stub(http, 'request')
+      .returns(mockRequest)
+      .callsArgWithAsync(1, mockResponse)
+
+    var server = http.createServer(function (req, res) {
+      res.statusCode = 500
+      res.end('NOT OK!')
+    })
+    
+    var port
+    var self = this
+    server.listen(function () {
+      port = server.address().port
+      
+      var options = {
+        port: port
+        , method: "GET"
+      }
+      
+      var r = new RequestDuplex(options)
+      r.on('error', function (err) {
+        (err instanceof Error).should.equal(true)
+        done()
+      })
+      
+      self.input.pipe(r).pipe(self.output)
+
+      setImmediate(function () {
+        mockResponse.emit('error', new Error())
+      })
+    })
+  })
+  
+})
+
